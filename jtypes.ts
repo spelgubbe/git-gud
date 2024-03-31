5f43645d74acdd24303dbf4896aa65f5dc43ebdf
Jakob Kratz <jakob.kratz@digpro.se>
2024-03-31 22:03:54 +0200

    Remove old files

--- jtypes.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Expected use case is to uniquely identify a file associated with a git diff
- */
-export interface FilePathMapping {
-    oldFilePath: string;
-    newFilePath: string;
-}
--- ui.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-const targetElement = document.getElementById('diff');
-const diff2htmlUi = new Diff2HtmlUI(targetElement);
-diff2htmlUi.fileListToggle(true);
-diff2htmlUi.fileContentToggle();
-diff2htmlUi.synchronisedScroll();
-diff2htmlUi.highlightCode();
-});
